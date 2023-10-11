7f585e942a3346f80d26df58989fe1346706a1fe
Administrator <Administrator@172.31.9.108>
2023-10-11 06:41:30 +0000

    Initial Project

---
+++ src/test/java/testScripts/FileUploadTest.java
@@ -0,0 +1,22 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploadTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://blueimp.github.io/jQuery-File-Upload/");
+        WebElement addFile = driver.findElement(By.xpath("//input[@type='file']"));
+        String filePath = System.getProperty("user.dir")+"//download.png";
+        addFile.sendKeys(filePath);
+        driver.findElement(By.xpath("//span[text()='Start upload']")).click(); 
+        
+        
+	}
+
+}
